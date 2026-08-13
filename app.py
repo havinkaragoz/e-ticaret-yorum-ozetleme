d6b4ac57ffa38d27393c26fe4fd19b7a9a787189
Havin Karagöz <karagozhavin21@gmail.com>
2026-08-13 23:43:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if st.button("Analiz Et ve Özet Çıkar"):
         with st.spinner("Yapay zeka yorumları analiz ediyor..."):
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.0-flash",
+                    model="gemini-2.5-flash",
                     contents=prompt
                 )
                 st.subheader("📌 Gemini Satın Alma Özeti")
